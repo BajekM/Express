481544026426aa9b56679998668716bf16a236dc
Marcin Bajek <bajekmarcin@gmail.com>
2020-02-19 08:10:35 +0100

    Create contact form

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -32,6 +33,18 @@ app.get('/history', (req, res) => {
   res.render('history');
 });
 
+app.post('/contact/send-message', (req, res) => {
+
+  const { author, sender, title, message, image } = req.body;
+
+  if(author && sender && title && message && image) {
+    res.render('contact', { isSent: true }, {fileName: req.params.image});
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+
+});
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 })
